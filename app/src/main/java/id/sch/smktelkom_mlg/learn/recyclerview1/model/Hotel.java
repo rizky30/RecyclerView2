6196f2ea4c1578435ffbb9447e896f7275cfb7a2
Muhammad Rizky Ajie Soelstyo <muhammad_soelistyo_24rpl@student.smktelkom-mlg.sch.id>
2016-11-01 12:07:12 +0700

    RecyclerView List

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview1/model/Hotel.java
@@ -0,0 +1,20 @@
+package id.sch.smktelkom_mlg.learn.recyclerview1.model;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by john on 11/1/2016.
+ */
+public class Hotel
+{
+    public String judul;
+    public String deskripsi;
+    public Drawable foto;
+
+    public Hotel(String judul, String deskripsi, Drawable foto)
+    {
+        this.judul = judul;
+        this.deskripsi = deskripsi;
+        this.foto = foto;
+    }
+}
